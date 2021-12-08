47b63eeb95c70079b083d979231b27d71f2b62b8
Johannes Sattler <johannessattler01@gmail.com>
2021-12-08 12:39:15 +0100

    Product seed & data dump processing

---
+++ models/Product.model.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+const ObjectId = Schema.Types.ObjectId;
+
+const productSchema = new Schema(
+{
+    name: String,
+    brand: String,
+    image: String,
+    palmOil: String,
+    vegan: String,
+    vegetarian: String,
+    sweet: Boolean,
+    salty: Boolean,
+    organic: Boolean,
+    fat: String,
+    salt: String,
+    saturatedFat: String,
+    sugars: String,
+    ingredients: String,
+    nutriScore: String,
+    nutrtionTable: {
+        energy: Number,
+        energyUnit: String,
+        fat: Number,
+        fatUnit: String,
+        salt: Number,
+        saltUnit: String,
+        sugars: Number,
+        sugarsUnit: String,
+        proteins: Number,
+        proteinsUnit: String,
+        carbohydrates: Number,
+        carbohydratesUnit: String,
+        calcium: Number,
+        calciumUnit: String,
+        fiber: Number,
+        fiberUnit: String,
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Product = model("products", productSchema);
+
+module.exports = Product;
