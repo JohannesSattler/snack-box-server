a98e7cdc251c2766395f1d21a556e610c44dce63
Johannes Sattler <johannessattler01@gmail.com>
2021-12-14 11:40:08 +0100

    deploy

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ require("./config")(app);
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
+app.use((req, res, next) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/public/index.html");
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
