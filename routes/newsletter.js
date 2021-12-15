e8f64a9d2f06eb36c30bdf63cf9c10e81e2a9bfc
Johannes Sattler <johannessattler01@gmail.com>
2021-12-15 09:16:01 +0100

    Newsletter email, newsletter unsubscribe & new signup email

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Newsletter = require('../models/Newsletter.model')
+const { sendEmail, sendNewsletterMail } = require('../emailSender')
 
 router.patch("/newsletter/create-entry", async (req, res, next) => {
     try {
@@ -16,38 +17,51 @@ router.patch("/newsletter/create-entry", async (req, res, next) => {
         if(!emailRegex.test(email)) {
             return res.status(400).json({
                 errorMessage: {
-                    message: "The email doesnt seem to be valid.", 
+                    message: "The email doesnt seem to be valid.",
                     type: 'email'
                 }
             });
         }
         
         const alreadySubscriped = await Newsletter.findOne({email})
-        console.log(alreadySubscriped)
         
         if(alreadySubscriped) {
             return res.status(400).json({
                 errorMessage: {
-                    message: "Seems like you already subscribed to the Newsletter.", 
-                    type: 'email'
-                }
-            });
+                        message: "Seems like you already subscribed to the Newsletter.",
+                        type: 'email'
+                    }
+                });
         }
-        
+                    
+        const verificationEmail = (name, email) => {
+            const newMail = {
+                subject: 'Newsletter Incoming 😊',
+                plainText: '',
+                htmlText: sendNewsletterMail(name, process.env.ORIGIN + '/newsletter/' + email + '/delete'),
+            }
+            
+            return newMail
+        }
+                    
+        // SEND MAIL
+        const {subject, plainText, htmlText} = verificationEmail(name, email)
+        sendEmail(email, subject, plainText, htmlText)
+
         const createdEmail = await Newsletter.create({ email, name })
         return res.status(201).json(createdEmail);
-    } 
+    }
     catch (error) {
         return res.status(404).json({ error: "Error in updating user" + error });
     }
 });
 
-router.delete("/newsletter/:email/delete", async (req, res, next) => {
+router.post("/newsletter/delete", async (req, res, next) => {
     try {
-        const {email} = req.params
+        const {email} = req.body
         const deletedEmail = await Newsletter.findOneAndDelete({ email })
         return res.status(201).json(deletedEmail);
-    } 
+    }
     catch (error) {
         return res.status(404).json({ error: "Error in updating user" + error });
     }
